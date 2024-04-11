340d4b63d8809fa975aaaecef587b4babdf1f58c
sarah_s <reed.se01@gmail.com>
2024-04-11 17:44:57 -0400

    updated button

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
 var button = document.querySelector(".show-cat");
-//console.log(button);
-
 var cat = document.querySelector(".cat");
-//console.log(cat);
 
 button.addEventListener("click", function (){
-    cat.classList.add("show");
+    if (cat.classList.contains("show"))
+    {
+    //console.log("yes");
+    cat.classList.remove("show");
+    button.innerText = "Wait, come back!";
+    button.classList.add = ("disappear");
+} 
+else 
+{ 
+//console.log("no");
+cat.classList.add("show");
+button.innerText = "Shoo, cat!";
+button.classList.remove = ("disappear");
+}
 });
